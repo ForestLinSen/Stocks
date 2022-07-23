6f1d32f957659d14dbfb4c06c8a0ffe3b24f0279
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-23 12:50:25 +0800

    Initial commit

---
+++ Stocks/Controllers/WatchListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocks
+//
+//  Created by Sen Lin on 21/7/2022.
+//
+
+import UIKit
+
+class WatchListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
