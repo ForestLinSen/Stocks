c0144f854cea9b7c9eb5a5151761cc5e6e4b6d2e
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-25 22:43:48 +0800

    app icon and search VC setting up

---
+++ Stocks/Controllers/SearchResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchResultViewController.swift
+//  Stocks
+//
+//  Created by Sen Lin on 25/7/2022.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class WatchListViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
+        view.backgroundColor = .systemBackground
+        setUpSearchViewController()
     }
-
-
+    
+    private func setUpSearchViewController(){
+        let resultVC = SearchResultViewController()
+        let searchVC = UISearchController(searchResultsController: resultVC)
+        searchVC.searchResultsUpdater = self
+        navigationItem.searchController = searchVC
+    }
+    
 }
 
+extension WatchListViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+              let resultVC = searchController.searchResultsController as? SearchResultViewController else {
+            print("Debug: cannot get query or result VC")
+            return
+        }
+        
+        // Optimize to reduce API call
+        
+        // Call API call to search
+        
+        // Update the results controller
+    }
+    
+    
+}
