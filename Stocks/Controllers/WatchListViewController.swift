c1d7de283f428b26ac40ff952a4b959eaa5a82a5
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-07-26 14:14:23 +0800

    title view

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -9,17 +9,31 @@ import UIKit
 
 class WatchListViewController: UIViewController {
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setUpSearchViewController()
+        setUpTitleView()
     }
+    
+    // MARK: - Private Functions
     
     private func setUpSearchViewController(){
         let resultVC = SearchResultViewController()
         let searchVC = UISearchController(searchResultsController: resultVC)
         searchVC.searchResultsUpdater = self
         navigationItem.searchController = searchVC
+    }
+    
+    private func setUpTitleView(){
+        let titleView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: navigationController?.navigationBar.frame.height ?? 100))
+        let titleLabel = UILabel(frame: CGRect(x: 10, y: 5, width: titleView.frame.width, height: titleView.frame.height - 15))
+        titleLabel.text = "Stocks"
+        titleLabel.font = .systemFont(ofSize: 32, weight: .semibold)
+        titleView.addSubview(titleLabel)
+        navigationItem.titleView = titleView
     }
     
 }
