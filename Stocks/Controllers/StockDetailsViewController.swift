24d79a340966aa182477cd70f9612a5018ef92a9
Wyman Lin <41566276+ForestLinSen@users.noreply.github.com>
2022-09-17 21:03:10 +0800

    stock details

--- Stocks/Controllers/StockDetailsViewController.swift
+++ Stocks/Controllers/StockDetailsViewController.swift
@@ -6,12 +6,22 @@
 //
 
 import UIKit
+import SafariServices
 
 class StockDetailsViewController: UIViewController {
+    
     // MARK: - Properties
     private let symbol: String
     private let companyName: String
     private let candleStickData: [CandleStick]
+    private var stories: [NewsStory] = []
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NewsHeaderView.self, forHeaderFooterViewReuseIdentifier: NewsHeaderView.identifier)
+        table.register(NewsStoryTableViewCell.self, forCellReuseIdentifier: NewsStoryTableViewCell.identifier)
+        return table
+    }()
     
     // MARK: - Init
     init(symbol: String, companyName: String, candleStickData: [CandleStick]) {
@@ -27,8 +37,104 @@ class StockDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .systemBackground
+        
+        setUpTable()
+        fetchFinancialData()
+        fetchNews()
+
+        
+        // Show view
+        // Financial Data
+        // Show Chart / Graph
+        // Show News
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func setUpTable() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.addSubview(tableView)
+    }
+    
+    func fetchFinancialData() {
+        renderChart()
+    }
+    
+    func fetchNews() {
+        APICaller.shared.fetchNews(for: .company(symbol: symbol)) {[weak self] result in
+            switch result {
+            case .success(let stories):
+                DispatchQueue.main.async {
+                    self?.stories = stories
+                    self?.tableView.reloadData()
+                }
+            case .failure(let failure):
+                print("Debug: cannot fetch data: \(failure)")
+            }
+        }
+    }
+    
+    func renderChart() {
+        
     }
 
+}
+
+
+extension StockDetailsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stories.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NewsStoryTableViewCell.identifier, for: indexPath) as? NewsStoryTableViewCell else {
+            fatalError()
+        }
+        
+        cell.configure(with: .init(model: stories[indexPath.row]))
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let cell = tableView.dequeueReusableHeaderFooterView(withIdentifier: NewsHeaderView.identifier) as? NewsHeaderView else {
+            return nil
+        }
+        
+        cell.delegate = self
+        cell.configure(with: .init(title: symbol,
+                                   shouldShownAddButton: true))
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return NewsStoryTableViewCell.preferredHeight
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return NewsHeaderView.preferredHeight
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        guard let url = URL(string: stories[indexPath.row].url) else { return }
+        let vc = SFSafariViewController(url: url)
+        present(vc, animated: true)
+    }
+    
+}
+
+extension StockDetailsViewController: NewsHeaderViewDelegate {
+    func newsHeaderViewDidTapButton(_ headerView: NewsHeaderView) {
+        
+    }
+    
+    
 }
